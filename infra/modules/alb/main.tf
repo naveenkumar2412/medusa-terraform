5f38b3364e06fc8aafe84bff53c0a4097ffbe1b4
Naveenkumar <naveenshanmugam198@gmail.com>
2025-05-29 10:44:55 +0530

    Update main.tf

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lb" "alb" {
-  name               = "medusa-alb"
+  name               = var.aws_lb_name
   internal           = false
   load_balancer_type = "application"
   subnets            = var.public_subnet_ids
@@ -7,7 +7,7 @@ resource "aws_lb" "alb" {
 }
 
 resource "aws_lb_target_group" "tg" {
-  name     = "medusa-tg"
+  name     = var.lb_target_group_name
   port     = 9000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
